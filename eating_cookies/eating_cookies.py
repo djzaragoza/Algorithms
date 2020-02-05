6ebe38b866cba8b3d19df5e64d3686462c306a95
unknown <dj.zaragoza@icloud.com>
2020-02-04 17:54:42 -0800

    working on project

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,7 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
+def eating_cookies_1(n, cache=None):
       if n < 2:
             return 1
       elif n == 2:
